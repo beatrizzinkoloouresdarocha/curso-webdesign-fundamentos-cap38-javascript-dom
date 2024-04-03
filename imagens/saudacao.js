9abc1b21fbbbe3ef36d41c5dbed0462248dc73ef
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-03 16:23:58 -0300

    finalizada mais uma etapa de saudacao js com mais informações.

--- imagens/saudacao.js
+++ imagens/saudacao.js
@@ -1,5 +1,7 @@
 (function(){
     const nomeUsuario= "Daniel"
-    document.querySelector(".top-bar p").textContent ="Bem-vindo(a)," +nomeUsuario
+    //document.querySelector(".top-bar p").textContent ="Bem-vindo(a)," +nomeUsuario
+    const elemento =document.querySelector(".top-bar p")
+    elemento.textContent ="Bem-vindo(a)," +nomeUsuario
 })()
 
