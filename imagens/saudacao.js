e0215a737356be1c846d85dac47ae7fb551be219
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-03 16:39:13 -0300

    foi documentado o codigo e colocado ele todo dentro  .

--- imagens/saudacao.js
+++ imagens/saudacao.js
@@ -1,9 +1,12 @@
 (function(){
-    const nomeUsuario= "Daniel"
-    //document.querySelector(".top-bar p").textContent ="Bem-vindo(a)," +nomeUsuario
+    const nomeUsuario= null
+    if(nomeUsuario){
+         //document.querySelector(".top-bar p").textContent ="Bem-vindo(a)," +nomeUsuario
     const elemento =document.querySelector(".top-bar p")
     console.log(elemento.textContent)
    // elemento.textContent ="Bem-vindo(a)," +nomeUsuario
    elemento.innerHTML += "<b>" + nomeUsuario +"</b>"
+    }
+    
 })()
 
