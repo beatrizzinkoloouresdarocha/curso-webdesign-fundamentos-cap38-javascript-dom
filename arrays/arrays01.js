0c0b239054106a3278739dd7814619aa43c4e85d
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-04 11:35:33 -0300

    consegui finalizar as duas etapas do exercicio da aula pratica de  arrays01.

--- arrays/arrays01.js
+++ arrays/arrays01.js
@@ -1,7 +1,8 @@
 const arr =[1,5,10,"olá",true]
 
-let sohNumeros =arr.every(function(){
-    return true
+let sohNumeros =arr.every(function(el){
+    console.log(el)
+    return typeof el === "number"
 })
 
 console.log(sohNumeros)
