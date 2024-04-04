e38ab36da611b35b6bea5eb066a29ae52fe38351
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-04 15:03:10 -0300

    finalizei a primeira parte do exrcicio concluido e coloquei pra rodar ] deu certo funcionou e deu o valor correto.

--- arrays/desafio.js
+++ arrays/desafio.js
@@ -1,6 +1,12 @@
 function sum (){
-
+   // const numbers =Array.from(arguments)
+    const numbers = [...arguments]
+    return numbers .reduce(function(sum,atual){
+        return sum + atual
+    } ,0)
 }
 function avarege(){
-    
+    return 0
 }
+ let soma=sum(1,2,3,4,5)
+ console.log(soma)
