67b9f698cf16455ece9460600b02ec89da614186
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-04 16:17:05 -0300

    finalizei mais uma etapa da atividade pratica .

--- arrays/desafio-ESS.js
+++ arrays/desafio-ESS.js
@@ -1,8 +1,13 @@
 function sum (){
     const numbers =[]
     for (let i = 0; i  <arguments.length;i++){
-        numbers.push(arguments[i])
-    }
+    numbers.push(arguments[i])
+   }
     console.log(numbers)
+
+   //Array.prototype
 }
-sum(1,2,3,4,5)
+ //sum(1,2,3,4,5)
+
+ //sum.call (null,1,2,3,4,5)
+ sum.apply(null,[1,2,3,4,5])
